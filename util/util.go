5860ccb922259fed27d94e950505e652ddcdabc6
jawr <jess@lawrence.pm>
2014-12-09 18:32:33 +0100

    Moved helper functions from parser to their own package.

---
+++ util/util.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"log"
+	"time"
+)
+
+func Trace() time.Time {
+	return time.Now()
+}
+
+func Un(t time.Time) {
+	elapsed := time.Since(t)
+	log.Printf("%s", elapsed.String())
+}
+
+func FilterIN(sl []string) []string {
+	fn := func(s string) bool {
+		if s == "in" {
+			return true
+		}
+		return false
+	}
+	return filter(sl, fn)
+}
+
+func filter(sl []string, fn func(string) bool) []string {
+	outi := 0
+	res := sl
+	for _, v := range sl {
+		if !fn(v) {
+			res[outi] = v
+			outi++
+		}
+	}
+	return res[0:outi]
+}
