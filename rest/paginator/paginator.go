970a01e2f4a80d7ec0c4f96767bf9e9de0848e96
jawr <jess@lawrence.pm>
2014-12-14 00:37:15 +0100

    Added paginator package.

---
+++ rest/paginator/paginator.go
@@ -0,0 +1,28 @@
+package paginator
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func getInt(s string, i int) int {
+	if len(s) > 0 {
+		if j, err := strconv.ParseInt(s, 10, 32); err == nil {
+			i = int(j)
+		}
+	}
+	return i
+}
+
+func Paginate(fn func(http.ResponseWriter, *http.Request, map[string][]string, int, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := r.URL.Query()
+		limit := getInt(params.Get("limit"), 15)
+		if limit > 50 {
+			// log?
+			limit = 50
+		}
+		idx := limit * getInt(params.Get("page"), 0)
+		fn(w, r, params, idx, limit)
+	}
+}
