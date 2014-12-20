99189ba0eded1c629843eacbb07d360ff0410a56
jawr <jess@lawrence.pm>
2014-12-20 01:11:17 +0100

    Added database/models/zonefile/parser

---
+++ database/models/zonefile/parser/get.go
@@ -0,0 +1,84 @@
+package parser
+
+import (
+	"fmt"
+	"github.com/jawr/dns/database/connection"
+	"github.com/jawr/dns/database/models/tld"
+	"net/url"
+	"strings"
+)
+
+const (
+	SELECT string = "SELECT * FROM parser "
+)
+
+func GetAll() string {
+	return SELECT
+}
+
+func GetByID() string {
+	return SELECT + "WHERE id = $1"
+}
+
+func parseRow(row connection.Row) (Parser, error) {
+	p := Parser{}
+	var tldID int32
+	err := row.Scan(&p.ID, &p.Filename, &p.Started, p.Finished, &p.Date, &tldID, &p.Logs)
+	if err != nil {
+		return p, err
+	}
+	t, err := tld.Get(tld.GetByID(), tldID)
+	p.TLD = t
+	return p, err
+}
+
+func Search(params url.Values, idx, limit int) ([]Parser, error) {
+	query := GetAll()
+	var where []string
+	var args []interface{}
+	i := 1
+	for k, _ := range params {
+		switch k {
+		case "name":
+		case "id":
+			where = append(where, fmt.Sprintf(k+" = $%d", i))
+			args = append(args, params.Get(k))
+			i++
+		}
+	}
+	if len(where) > 0 {
+		query += "WHERE " + strings.Join(where, " AND ") + " "
+	}
+	query += fmt.Sprintf("LIMIT %d OFFSET %d", limit, idx)
+	return GetList(query, args...)
+}
+
+func Get(query string, args ...interface{}) (Parser, error) {
+	conn, err := connection.Get()
+	if err != nil {
+		return Parser{}, err
+	}
+	row := conn.QueryRow(query, args...)
+	return parseRow(row)
+}
+
+func GetList(query string, args ...interface{}) ([]Parser, error) {
+	conn, err := connection.Get()
+	if err != nil {
+		return []Parser{}, err
+	}
+	rows, err := conn.Query(query, args...)
+	defer rows.Close()
+	if err != nil {
+		return []Parser{}, err
+	}
+	var list []Parser
+	for rows.Next() {
+		rt, err := parseRow(rows)
+		if err != nil {
+			return list, err
+		}
+		list = append(list, rt)
+	}
+	return list, rows.Err()
+}
---
+++ database/models/zonefile/parser/parser.go
@@ -0,0 +1,102 @@
+package parser
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/jawr/dns/database/connection"
+	"github.com/jawr/dns/database/models/tld"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type Log struct {
+	Time    time.Time `json:"time"`
+	Message string    `json:"message"`
+}
+
+type Parser struct {
+	ID       int32      `json:"id"`
+	Filename string     `json:"filename"`
+	Started  time.Time  `json:"started"`
+	Finished *time.Time `json:"finished"`
+	Date     time.Time  `json:"date"`
+	TLD      tld.TLD    `json:"tld"`
+	Logs     []Log      `json:"logs"`
+}
+
+var tldRE *regexp.Regexp = regexp.MustCompile(`^(\d{8})\-([\w\d\-]+)[\-\.]zone[\-\.](data|gz)`)
+
+const TLD_FILENAME_DATE string = "20060102"
+
+func New(filename string) (Parser, error) {
+	args := strings.Split(filename, "/")
+	name := args[len(args)-1]
+	args = tldRE.FindStringSubmatch(name)
+	p := Parser{}
+	if len(args) < 4 {
+		return p, errors.New("No TLD or date detected in zone filename: " + filename)
+	}
+	t, err := tld.Get(tld.GetByName(), args[2])
+	if err != nil {
+		return p, err
+	}
+	date, err := time.Parse(TLD_FILENAME_DATE, args[1])
+	if err != nil {
+		return p, err
+	}
+	p.Filename = name
+	p.TLD = t
+	p.Date = date
+	return p, nil
+}
+
+func (p *Parser) Insert() error {
+	conn, err := connection.Get()
+	if err != nil {
+		return err
+	}
+	var id int32
+	err = conn.QueryRow("INSERT INTO parser (filename, parser_date, tld) VALUES ($1, $2, $3) RETURNING id",
+		p.Filename,
+		p.Date,
+		p.TLD.ID,
+	).Scan(&id)
+	p.ID = id
+	return err
+
+}
+
+func (p Parser) Save() error {
+	conn, err := connection.Get()
+	if err != nil {
+		return err
+	}
+	b, err := json.Marshal(p.Logs)
+	if err != nil {
+		return err
+	}
+	_, err = conn.Exec("UPDATE parser SET logs = $1 WHERE id = $2", string(b), p.ID)
+	return err
+}
+
+func (p *Parser) Update(msg string, args ...interface{}) error {
+	log := Log{
+		Time:    time.Now(),
+		Message: fmt.Sprintf(msg, args...),
+	}
+	p.Logs = append(p.Logs, log)
+	return p.Save()
+}
+
+func (p *Parser) Finish() error {
+	conn, err := connection.Get()
+	if err != nil {
+		return err
+	}
+	t := time.Now()
+	p.Finished = &t
+	_, err = conn.Exec("UPDATE parser SET finished_at = $1 WHERE id = $2", p.Finished, p.ID)
+	return err
+}
